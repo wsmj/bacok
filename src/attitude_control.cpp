f75cc1755e6c908da89d8eace912bbdb74e34eb0
zarezza <nfahreza01@gmail.com>
2024-12-05 21:49:26 +0700

    add pose

--- src/attitude_control.cpp
+++ src/attitude_control.cpp
@@ -3,16 +3,21 @@
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/msg/vehicle_attitude_setpoint.hpp>
 #include <px4_msgs/msg/sensor_gps.hpp>
+#include <vision_msgs/msg/pose2_d.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <cmath>
 #include <stdint.h>
 
+#include <bacok/frameTransport.h>
+
 #include <chrono>
 #include <iostream>
+
 
 using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
+using namespace vision_msgs::msg;
 using std::placeholders::_1;
 
 class OffboardControl : public rclcpp::Node
@@ -22,11 +27,13 @@ public:
 	{
 		rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+		
 		offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
 		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
 		vehicle_attitude_setpoint_publisher_ = this->create_publisher<VehicleAttitudeSetpoint>("/fmu/in/vehicle_attitude_setpoint", 10);
-		sensor_combined_subscriber_ = this->create_subscription<SensorGps>("/fmu/out/vehicle_gps_position", qos, std::bind(&OffboardControl::sensor_combined_callback, this, _1));
 		
+		sensor_combined_subscriber_ = this->create_subscription<SensorGps>("/fmu/out/vehicle_gps_position", qos, std::bind(&OffboardControl::sensor_combined_callback, this, _1));
+		target_frame_position_subscriber_ = this->create_subscription<Pose2D>("/target_frame_location", qos, std::bind(&OffboardControl::target_location_callback, this, _1));
 
 		offboard_setpoint_counter_ = 0;
 
@@ -56,6 +63,7 @@ private:
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
 	rclcpp::Publisher<VehicleAttitudeSetpoint>::SharedPtr vehicle_attitude_setpoint_publisher_;
 	rclcpp::Subscription<SensorGps>::SharedPtr sensor_combined_subscriber_;
+	rclcpp::Subscription<Pose2D>::SharedPtr target_frame_position_subscriber_;
 
 	std::atomic<uint64_t> timestamp_;   
 
@@ -63,14 +71,14 @@ private:
 
 	void publish_offboard_control_mode();
 	void publish_vehicle_attitude_setpoint();
-	void loiter_control();
-
 	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0, float param3 = 0.0, float param4 = 0.0, float param5 = 0.0, float param6 = 0.0, float param7 = 0.0);
 
 	SensorGps::SharedPtr sensor_data;
 	void sensor_combined_callback(const SensorGps::SharedPtr msg) {sensor_data = msg;}
-};
 
+	Pose2D::SharedPtr target_frame_position;
+	void target_location_callback(const Pose2D::SharedPtr msg) {target_frame_position = msg;}
+};
 
 void OffboardControl::publish_offboard_control_mode()
 {
@@ -84,23 +92,47 @@ void OffboardControl::publish_offboard_control_mode()
 	offboard_control_mode_publisher_->publish(msg);
 }
 
-
 void OffboardControl::publish_vehicle_attitude_setpoint()
 {
-    VehicleAttitudeSetpoint msg{};
 
-    double roll_value = sin(static_cast<double>(this->get_clock()->now().nanoseconds() / 1000000) / 2000);  // Adjust time scaling if needed
+	if (!target_frame_position) {
+		RCLCPP_WARN(this->get_logger(), "No target frame position received yet.");
+		return; // Exit the function if no data has been received
+	}
+	
+	VehicleAttitudeSetpoint msg{};
 
-    double half_roll = roll_value / 3.0;
-    msg.q_d = {float(cos(half_roll)), float(sin(half_roll)), 0.0, 0.0};  // Quaternion for roll (q_d = [w, x, y, z])
+	float roll;
 
-    msg.thrust_body = {0.35, 0.0, 0.0};
+	int target_x = target_frame_position->position.x;
 
-    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+	if (target_x == 640) roll = 0;
+	else {
+		int direction = (target_x < 640) ? -1 : 1;
+		double ratio = static_cast<double>(std::abs(target_x - 640)) / 640.0;
+		roll = direction * (8.0 * ratio);
+	}
 
-    vehicle_attitude_setpoint_publisher_->publish(msg);
+	float pitch;
+	int target_y = target_frame_position->position.y;
 
-    RCLCPP_INFO(this->get_logger(), "Published roll: %f radians (converted to quaternion)", roll_value);
+	if (target_y == 360) pitch = 0;
+	else {
+		int direction = (target_y < 360) ? 1 : -1;
+		double ratio = static_cast<double>(std::abs(target_y - 360)) / 360.0;
+		pitch = direction * (5.0 * ratio);
+	}
+
+	RCLCPP_INFO(this->get_logger(), "Euler: %f, %f, %f", roll, pitch, 0.0);
+	FrameTransport::Quaternion q = FrameTransport::toQuaternion({roll, pitch, 0.0});
+	RCLCPP_INFO(this->get_logger(), "Quaternion: %f %f %f %f", q.w, q.x, q.y, q.z);
+
+	msg.q_d = {float(q.w), float(q.x), float(q.y), float(q.z)};
+		
+	msg.thrust_body = {0.35, 0.0, 0.0};
+	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+
+	vehicle_attitude_setpoint_publisher_->publish(msg);
 }
 
 
